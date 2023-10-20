43838c3fcaa668e0db102f78d1ced5f29b8200fe
Mariangithubit <metsyb1@hotmail.com>
2023-10-20 16:20:32 -0700

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-""" """
+"""script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """print welcome page"""
+    return 'Hello HBNB!'
+
+@pp.route('/hbnb')
+def hbnb():
+    """print another word"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
