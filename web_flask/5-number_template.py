30f507c68c4a0ff2a77910f3dbc0511c9973523d
Mariangithubit <metsyb1@hotmail.com>
2023-10-21 12:16:03 -0700

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """print welcome page"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """print another word"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """print C followed by the value of the text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """Display “Python ”, followed by the value of the text variable"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def integer(n):
+    """display number if is an integer"""
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Display HTML page only if n is an integer"""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
